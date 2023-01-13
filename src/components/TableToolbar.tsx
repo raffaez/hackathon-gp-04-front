d538bc9f8023ea7e2db00ced45faf7142fd25356
raffaez <rafaeleasouzas@gmail.com>
2023-01-12 22:24:59 -0300

    refactor: make toolbar a sepparate component

---
+++ src/components/TableToolbar.tsx
@@ -0,0 +1,42 @@
+import { AddCircleOutlineRounded, FilterAltRounded } from '@mui/icons-material';
+import { IconButton, Tooltip, Typography } from '@mui/material';
+import Toolbar, { ToolbarProps } from '@mui/material/Toolbar/Toolbar';
+import React from 'react';
+
+interface TableToolbarProps extends ToolbarProps {
+  handleOpenFiltrar: () => void;
+}
+
+function TableToolbar(props: TableToolbarProps) {
+  const { handleOpenFiltrar } = props;
+  
+  return (
+    <Toolbar>
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          variant="h5"
+          id="tb_projetos"
+          component="div"
+        >
+          Projetos
+        </Typography>
+        
+        <Tooltip title="Filtrar"
+        >
+          <IconButton
+            onClick={handleOpenFiltrar}
+          >
+            <FilterAltRounded />
+          </IconButton>
+        </Tooltip>
+
+        <Tooltip title="Adicionar projeto">
+          <IconButton>
+            <AddCircleOutlineRounded />
+          </IconButton>
+        </Tooltip>
+    </Toolbar>
+  );
+}
+
+export default TableToolbar
